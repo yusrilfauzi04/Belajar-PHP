46b0056f37e39da6b8652392c808d036efcd75ac
yusril fauzi <227006067@student.unsil.ac.id>
2023-09-15 15:44:49 +0700

    bootcamp

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        echo "<h1><b>BIODATA</b></h1>";
+        echo "<table border='1'>";
+        echo "<tr><td>Nama</td><td>:</td><td>Muhammad Yusril Fauzi</td></tr>";
+        echo "<tr><td>Tempat Lahir</td><td>:</td><td>Ciamis</td></tr>";
+        echo "<tr><td>Tanggal Lahir</td><td>:</td><td>04 Desember 2003</td></tr>";
+        echo "<tr><td>Hobi</td><td>:</td><td>Nonton Film, Main game, Traveling</td></tr>";
+        echo "</table>";
+    ?>
+</body>
+</html>
